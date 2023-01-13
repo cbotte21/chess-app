eb17ed4cd13029869e5d0561d7d2c1f282259cc8
cbotte21 <cbotte21@Ubuntu.myguest.virtualbox.org>
2023-01-12 20:21:53 -0500

    fixed enviroment

--- servers/github.com/cbotte21/hive-go/cmd/build_pb.sh
+++ servers/github.com/cbotte21/hive-go/cmd/build_pb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-protoc --go_out=$SERVER_DIR --proto_path=$PROJ_DIR/proto/ hive.proto
+protoc --go_out=$SERVER_DIR --go_opt=paths=source_relative --go-grpc_out=$SERVER_DIR --go-grpc_opt=paths=source_relative --proto_path=$PROJ_DIR/proto/ hive.proto
